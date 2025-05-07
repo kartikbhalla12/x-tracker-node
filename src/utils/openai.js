d580ebdf134f3f34c727563c55b0d9d7d2264ed5
Kartik Bhalla <kartikbhalla12@gmail.com>
2025-05-07 23:53:34 +0530

    src: openai: add 5sec timeout
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import OpenAI from "openai";
 
 import logger from "@utils/logger.js";
 
 const OPENAI_PROMPT_DOC_ID = process.env.OPENAI_PROMPT_DOC_ID;
+const OPENAI_ENDPOINT = process.env.OPENAI_ENDPOINT;
 
 let systemPrompt = null;
 const loadPromptFromGoogleDocs = async () => {
@@ -22,54 +22,59 @@ const loadPromptFromGoogleDocs = async () => {
 };
 loadPromptFromGoogleDocs();
 
+const controller = new AbortController();
+
 export const analyzeTweet = async (tweetText, tweetImageUrl, openAIKey) => {
-  const openai = new OpenAI({
-    apiKey: openAIKey,
-    dangerouslyAllowBrowser: true,
-  });
-
-  const customPrompt = `
-  ${systemPrompt}
-  
-  Analyze the following tweet and attached image URL carefully and return a JSON object with the token name and ticker.
-    Tweet: ${tweetText || "[No tweet text provided]"}
-    Image URL: ${tweetImageUrl || "[No image URL provided]"}
-  `;
-
   if (!systemPrompt) {
     logger.error("System prompt not loaded");
     return null;
   }
 
-  try {
-    const completion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: [
-            {
-              type: "text",
-              text: customPrompt,
-            },
-            ...(tweetImageUrl
-              ? [
-                  {
-                    type: "image_url",
-                    image_url: { url: tweetImageUrl },
-                  },
-                ]
-              : []),
-          ],
-        },
-      ],
-      model: "gpt-4o",
-      temperature: 0.3,
-      response_format: {
-        type: "json_object",
-      },
-    });
+  const customPrompt = `
+    ${systemPrompt}
+    
+    Analyze the following tweet and attached image URL carefully and return a JSON object with the token name and ticker.
+    
+    Tweet: ${tweetText || "[No tweet text provided]"}
+    Image URL: ${tweetImageUrl || "[No image URL provided]"}
+    `;
 
-    const response = completion.choices[0]?.message?.content;
+  const messages = [
+    {
+      role: "user",
+      content: [
+        { type: "text", text: customPrompt },
+        ...(tweetImageUrl
+          ? [
+              {
+                type: "image_url",
+                image_url: { url: tweetImageUrl },
+              },
+            ]
+          : []),
+      ],
+    },
+  ];
+
+  try {
+    const { data } = await axios.post(
+      OPENAI_ENDPOINT,
+      {
+        model: "gpt-4o",
+        messages,
+        temperature: 0.3,
+        response_format: {
+          type: "json_object",
+        },
+      },
+      {
+        headers: { Authorization: `Bearer ${openAIKey}` },
+        timeout: 5000,
+        signal: controller.signal,
+      }
+    );
+
+    const response = data.choices[0]?.message?.content;
     if (!response) {
       throw new Error("No response from OpenAI");
     }
